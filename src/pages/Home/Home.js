8c6f169943252dbaefd730c47846ef90d579061c
홍지영 <hjy961021@gmail.com>
2024-01-26 22:10:00 +0900

    feat : 싸이월드 메인 추가

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import HomeLayout from '../../components/HomeLayout';
+import Tab from '../../components/Tab';
+import HomeLeft from './HomeLeft';
+import HomeRight from './HomeRight';
 
 const Home = () => {
   return (
-    <HomeLayout
-      leftChildren={<div className="bottomArea left">ddd</div>}
-      rightChildren={<div className="bottomArea right">fdafdfa</div>}
-    />
+    <>
+      <HomeLayout
+        leftChildren={
+          <div className="bottomArea left">
+            {/* ex) 사진첩 클릭 했을 때 homeLeft와 사진첩Left로 component변경변경 */}
+            <HomeLeft />
+          </div>
+        }
+        rightChildren={
+          <div className="bottomArea right">
+            {/* ex) 사진첩 클릭 했을 때 homeRight와 사진첩Right로 component변경 */}
+            <HomeRight />
+          </div>
+        }
+      />
+      <Tab />
+    </>
   );
 };
 
