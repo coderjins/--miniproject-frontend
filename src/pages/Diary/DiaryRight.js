98d8609fae19b95b400de4dbe45ef348408d32fe
홍지영 <hjy961021@gmail.com>
2024-02-29 15:08:05 +0900

    feat : 폴더 구조 추가

---
+++ src/pages/Diary/DiaryRight.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DiaryRight = () => {
+  return <div />;
+};
+
+export default DiaryRight;
---
+++ src/pages/Picture/PictureLeft.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PictureLeft = () => {
+  return <div />;
+};
+
+export default PictureLeft;
